6da9768c2b06de69cd46c3875184e8ae9b3fe1aa
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-09-26 16:29:11 +0200

    Update micro_test.go

--- micro_test.go
+++ micro_test.go
@@ -2,6 +2,7 @@ package micro_test
 
 import (
 	"context"
+	"fmt"
 	. "github.com/bukowa/micro"
 	"log"
 	"testing"
@@ -15,7 +16,7 @@ const (
 )
 
 func BenchmarkNew(t *testing.B) {
-	micro := New(1, func(i int, m *Micro) Event {
+	micro := NewMicro(1, func(i int, m *Micro) Event {
 		return SuccessEvent
 	})
 	micro.Start()
@@ -26,7 +27,7 @@ func BenchmarkNew(t *testing.B) {
 func TestNewWithContext(t *testing.T) {
 	defer testTime(t, time.Now(), time.Second/2, 50)
 	ctx, canc := context.WithCancel(context.Background())
-	m := NewWithContext(ctx, 1, func(i int, m *Micro) Event {
+	m := NewMicroWithContext(ctx, 1, func(i int, m *Micro) Event {
 		return SuccessEvent
 	})
 	m.Start()
@@ -34,11 +35,12 @@ func TestNewWithContext(t *testing.T) {
 	time.Sleep(time.Second/2)
 	canc()
 	m.Wait()
+	m.Wait()
 }
 
 func TestMicro_Hooks(t *testing.T) {
 
-	micro1 := New(1, func(i int, m *Micro) Event {
+	micro1 := NewMicro(1, func(i int, m *Micro) Event {
 		log.Print("hello from micro1!")
 		return DoneEvent
 	})
@@ -64,6 +66,54 @@ func TestMicro_Hooks(t *testing.T) {
 	micro1.WaitFor(time.Millisecond)
 	log.Print(micro1.Start())
 	micro1.Wait()
+}
+
+func ExampleMicro_Start() {
+	const Success Event = "success"
+	const Failed Event = "failed"
+
+	micro1 := NewMicro(1, func(i int, m *Micro) Event {
+		return Success
+	})
+
+	micro2 := NewMicro(1, func(i int, m *Micro) Event {
+		fmt.Println("hello from micro2")
+		return Failed
+	})
+
+	micro1.RegisterHooks(map[Event][]func(m *Micro) func(){
+		AfterStart: {
+			func(m *Micro) func() {
+				return func() {
+					fmt.Println("hello from micro")
+					fmt.Println("starting micro2")
+					micro2.Start()
+				}
+			},
+		},
+	})
+
+	micro2.RegisterHooks(map[Event][]func(m *Micro) func(){
+		Failed: {
+			func(m *Micro) func() {
+				fmt.Println("hook registered")
+				return func() {
+					fmt.Println("stopping all micros")
+					micro1.Stop()
+					micro2.Stop()
+				}
+			},
+		},
+	})
+
+	micro1.Start()
+	micro1.Wait()
+	micro2.Wait()
+	// Output: hook registered
+	//hello from micro
+	//starting micro2
+	//hello from micro2
+	//stopping all micros
 }
 
 func testTime(t testing.TB, start time.Time, want, vary time.Duration) {
